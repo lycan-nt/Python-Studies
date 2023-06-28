52fabf98a043a55d925327e86d5c2920d4b05246
Felipe D. Santos <lycan.felipe@gmail.com>
2023-06-28 06:59:02 -0300

    Type Conversion

---
+++ PythonBeginners/HelloWorld/Exercises/Calc.py
@@ -0,0 +1,6 @@
+first_number = float(input("First: "))
+second_number = float(input("Second: "))
+
+result_sum = first_number + second_number
+
+print("Sum: ", result_sum)
---
+++ PythonBeginners/HelloWorld/TypeConversion.py
@@ -0,0 +1,13 @@
+birth_year = input("Enter your birth year: ")
+age = 2023 - int(birth_year)
+print(age)
+
+#convert to int
+int()
+#convert to float
+float()
+#convert to boolean
+bool()
+#convert to string
+str()
+
