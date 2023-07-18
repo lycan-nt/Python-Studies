9d7f1eb795eea438ed2336e097f14b80d4fed88d
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-17 22:27:16 -0300

    List Methods

---
+++ PythonBeginners/HelloWorld/ListMethods.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+numbers.insert(0, -1)
+print(numbers)
+numbers.remove(-1)
+print(numbers)
+print(1 in numbers)
+print(len(numbers))
+numbers.clear()
+print(numbers)
