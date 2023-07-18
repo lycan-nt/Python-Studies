aff3b2db44d21f127a5bc8e5f74d94454c7145e2
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-17 22:13:55 -0300

    While Loops

---
+++ PythonBeginners/HelloWorld/WhileLoop.py
@@ -0,0 +1,9 @@
+initNumber = 0
+while initNumber <= 5:
+    print(initNumber)
+    initNumber = initNumber + 1
+
+initNumber = 0
+while initNumber <= 5:
+    print(initNumber * '*')
+    initNumber = initNumber + 1
