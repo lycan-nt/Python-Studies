2db1cbf9d2f8a33d8780246db437baa92fe83cf9
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-17 21:33:29 -0300

    Ifs

---
+++ PythonBeginners/HelloWorld/IFs.py
@@ -0,0 +1,12 @@
+temperature = 25
+
+if temperature > 30:
+    print("It's a hot day")
+    print("I need a drink")
+elif temperature > 20:
+    print("It's a nice day")
+elif temperature > 10:
+    print("It's a bit cold")
+else:
+    print("It's cold")
+print("Done")
