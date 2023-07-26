fff8964df3ec0ae6b27a6ccca8f5ab1b670d9ba9
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-26 20:16:36 -0300

    Receveid Json in method Post

--- PythonWeb/Security/Authenticate/main.py
+++ PythonWeb/Security/Authenticate/main.py
@@ -1,7 +1,15 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Data(BaseModel):
+    name: str
+
+@app.post("/create/")
+async def create(data: Data):
+    return {"data": data}
 
 @app.get("/teste/{item_id}")
 async def test(item_id: str, query: int):
