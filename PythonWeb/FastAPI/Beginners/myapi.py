cab337952d7cd89d68a14ef17eb4c8fbbea33573
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-25 23:06:29 -0300

    Put Method

--- PythonWeb/FastAPI/Beginners/myapi.py
+++ PythonWeb/FastAPI/Beginners/myapi.py
@@ -17,6 +17,11 @@ class Student(BaseModel):
     age: int
     year: str
 
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
+
 @app.get("/")
 def index():
     return {"name": "First Data"}
@@ -42,3 +47,19 @@ def create(student_id: int, student: Student):
         return {"Error": "Student exists"}
     students[student_id] = student
     return students[student_id]
+
+@app.put("/update/{id}")
+def update(id: int, student: UpdateStudent):
+    if id not in students:
+        return {"Error": "Student does not exists"}
+    
+    if student.name != None:
+        students[id].name = student.name
+    
+    if student.age != None:
+        students[id].age = student.age
+
+    if student.year != None:
+        students[id].year = student.year
+
+    return students[id]
