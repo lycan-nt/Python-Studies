dbc29e1b86b11f26ba2eee4119a9d7f976b7e2e7
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-25 22:21:08 -0300

    Query Parameters

--- PythonWeb/FastAPI/Beginners/myapi.py
+++ PythonWeb/FastAPI/Beginners/myapi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 
@@ -21,3 +22,10 @@ def get_all_students():
 @app.get("/students/{id}")
 def get_student(id: int = Path(description="The ID of the student you want to view", gt=0, lt=10)):
     return students[id]
+
+@app.get("/get-by-name")
+def get_student(*, name: Optional[str] = None, test: int):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not Found"}
