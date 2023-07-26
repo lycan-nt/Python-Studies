8417ba85b44b8334bb0649bca47dd392a22e01da
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-25 21:54:10 -0300

    Path Parameters

--- PythonWeb/FastAPI/Beginners/myapi.py
+++ PythonWeb/FastAPI/Beginners/myapi.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+students = {
+    1: {
+        "name": "Felipe D. Santos",
+        "age": 27,
+        "class": "Code"
+    }
+}
 
 @app.get("/")
 def index():
     return {"name": "First Data"}
+
+@app.get("/students")
+def get_all_students():
+    return {"Students": len(students)}
+
+@app.get("/students/{id}")
+def get_student(id: int = Path(description="The ID of the student you want to view", gt=0, lt=10)):
+    return students[id]
